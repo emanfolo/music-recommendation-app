8ead03cb583f8897a88671a422ca529fb16b5fa4
Emmanuel <emmanuel@contact.xyz>
2023-11-21 00:15:26 +0000

    Complete firebase functions - Complete firebase functions on backend - Set up genre select page - Set up final API call to request spotify song data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,91 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 // import {onRequest} from "firebase-functions/v2/https";
 // import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
-import OpenAI from "openai"
+import OpenAI from "openai";
+import axios from "axios";
 
+// Change process.env to functions.config().spotify.client_id when time is right
 
-export const handleMood = functions.https.onCall(async (request) => {
+export const getSpotifySongs = functions.https.onCall(async (data, context) => {
+    // Step 0: Define Spotify API endpoint, retrieve Spotify client credentials and OpenAI key
+    const spotifyApiEndpoint = "https://api.spotify.com/v1/recommendations";
+    const clientId = process.env.SPOTIFY_CLIENT_ID || '';
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET || '';
+    const openAiKey = process.env.OPEN_AI_KEY || '';
+
+    if (!clientId || !clientSecret || !openAiKey) {
+        console.error("Missing required environment variable.");
+        return { error: "Internal Server Error" };
+    }
+
+    // Step 0.1: Initialize OpenAI with API key
     const openai = new OpenAI({
         apiKey: process.env.OPEN_AI_KEY,
     });
 
     try {
-        const userPrompt = `Generate a TailwindCSS color gradient that best describes the mood of ${request.mood}. Please format the output in JSON with the key 'backgroundColor'. If you cannot provide one, send me back the word "false". The css should be in tailwind format e.g. bg-gradient-to-r from-cyan-500 to-teal-500. The css should be able to pass the following regex: \b(bg-gradient-to-[a-z]+(?: from-[a-z]+)?(?: via-[a-z]+)?(?: to-[a-z]+)?).`
-        const systemPrompt = 'System, you are a CSS code generator assistant. Your task is to create a Tailwind CSS color gradient that best represents a given mood. The output should be in JSON format with a single key called backgroundColor. Provide a visually appealing gradient that aligns with the specified mood.'
+        // Step 1: Request music parameters from OpenAI based on user's mood
+        const userPrompt = `Generate music parameters (target_valence, target_energy, target_danceability) that match the mood of: ${data.mood}. Provide values that represent the desired emotional and energetic qualities of the music. Keep the response in JSON format.`;
+        const systemPrompt = "System, you are an AI music parameter generator. Your task is to generate music parameters, including target_valence, target_energy, and target_danceability, based on a user's description of their mood. The goal is to provide values that align with the emotional and energetic qualities mentioned by the user. Ensure that the response is in JSON format.";
 
         const { choices } = await openai.chat.completions.create({
             messages: [{ role: "user", content: userPrompt }, { role: "system", content: systemPrompt }],
-            model: "gpt-3.5-turbo",
+            model: "gpt-3.5-turbo"
         });
 
-        const responseData = choices[0]?.message?.content || 'No response from OpenAI';
+        const musicParams = choices[0]?.message?.content || 'No response from OpenAI';
+        const { target_valence, target_energy, target_danceability } = JSON.parse(musicParams);
 
-        return responseData;
-    } catch (error) {
-        console.error('Error calling OpenAI:', error);
-        return { error: 'Internal Server Error' };
+        try {
+            // Step 2: Obtain Client Credentials Access Token from Spotify
+            const authResponse = await axios.post(
+                "https://accounts.spotify.com/api/token",
+                null,
+                {
+                    params: {
+                        grant_type: "client_credentials",
+                    },
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    auth: {
+                        username: clientId || '',
+                        password: clientSecret || '',
+                    },
+                },
+            );
+
+            const accessToken = authResponse.data.access_token;
+
+            try {
+                // Step 3: Call the Spotify API using the obtained access token and music parameters
+                const spotifyResponse = await axios.get(spotifyApiEndpoint, {
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                    },
+                    params: {
+                        target_valence: target_valence,
+                        target_energy: target_energy,
+                        target_danceability: target_danceability,
+                        seed_genres: data.seedGenres,
+                        limit: 20,
+                    },
+                });
+
+                const recommendedTracks = spotifyResponse.data.tracks;
+                return {recommendedTracks, accessToken}; // potentially have to return access token also 
+            } catch (spotifyError) {
+                console.error("Error calling Spotify API:", spotifyError);
+                return { error: "Error calling Spotify API" };
+            }
+        } catch (authError) {
+            console.error("Error obtaining Spotify access token:", authError);
+            return { error: "Error obtaining Spotify access token" };
+        }
+    } catch (openaiError) {
+        console.error("Error calling OpenAI:", openaiError);
+        return { error: "Error calling OpenAI" };
     }
 });
 
--- music-recommendation/src/pages/player.tsx
+++ music-recommendation/src/pages/player.tsx
@@ -1,18 +1,80 @@
 import { EmbeddedYoutubePlayer } from "app/components";
 import "../app/globals.css";
+import { useEffect, useState } from "react";
+import { httpsCallable } from "firebase/functions";
+import { functions, init } from "../../firebase";
 
-const sampleBackgroundColor = {
-  backgroundColor: "bg-gradient-to-r from-yellow-300 to-orange-500",
-};
+const LoadingSpinner: React.FC = () => {
+  return <span className="loader"></span>;
+}; 
+
 const PlayerPage = () => {
+  const [playlist, setPlaylist] = useState<any>()
+  const [player, setPlayer] = useState(undefined);
+  const [is_paused, setPaused] = useState(false);
+const [is_active, setActive] = useState(false);
+const [current_track, setTrack] = useState(undefined);
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [dataLoading, setDataLoading] = useState(true)
+
+
+  useEffect(() => {
+    init().then(() => setLoading(false));
+  });
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+   
+    const mood = localStorage.getItem('moodInput')
+    const seedGenres = localStorage.getItem('seedGenres')
+
+    const requestSpotifyData = httpsCallable(functions, "getSpotifySongs");
+
+
+    try {
+      setDataLoading(true)
+
+      const response = await requestSpotifyData({ mood, seedGenres });
+
+      if (response.data === "No response from OpenAI") {
+        setError("There's been an issue with OpenAI, please try again.");
+      } else {
+        console.log("Successfully received response:", response.data);
+        setPlaylist(response)
+        localStorage.setItem('data', JSON.stringify(response))
+        const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+    
+
+      }
+
+    } catch (error) {
+      console.error("Error calling getSpotifySongs function:", error);
+      setError("There's been an issue with our server, please try again.");
+      // Cover errors properly 
+    } finally {
+      setDataLoading(false);
+    }
+  }
+  fetchData()
+
+  }, [])
+
+
+  
   return (
-    <div className=" h-full min-h-screen w-screen bg-gradient-to-r from-yellow-300 to-orange-500">
+    <div className=" h-full min-h-screen w-screen flex justify-center items-center bg-gray-200">
+      {dataLoading ? (<LoadingSpinner />) : (
       <div className=" flex justify-center items-center h-40">
-        <h1 className="text-xl">Curated Playlist based on your mood</h1>
+        <h1 className="text-xl text-black">Curated Playlist based on your mood</h1>
       </div>
-      <div className=" flex justify-center items-center h-auto">
-        <EmbeddedYoutubePlayer />
-      </div>
+
+      )}
     </div>
   );
 };
